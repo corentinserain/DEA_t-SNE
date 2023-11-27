3402a729b2d67d3f5691efd5e2b0cb37e015ff80
ecoutaud <131028820+ecoutaud@users.noreply.github.com>
2023-11-27 16:25:52 +0100

    Add files via upload

--- generate_data.py
+++ generate_data.py
@@ -5,25 +5,26 @@ import pandas as pd
 import os
 import numpy as np
 
-def parameters_mc(): 
-    n_samples = [100,200,300]
-    n_features = [10,20,30,40,50]
+def parameters_mc(samples, features): 
     n_classes = [2,3,4,5]
     p_informative = [0.5, 0.75, 1]
-    all_parameters = list(product(n_samples, n_features, n_classes, p_informative))
+    all_parameters = list(product(samples, features, n_classes, p_informative))
     return all_parameters
 
-def parameters_uni():
-    
+def parameters_uni(samples, features):
+    all_parameters = list(product(samples, features))
+    return all_parameters
 
 def scaler(dataset):
     scaler = MinMaxScaler()
     scaler.fit(dataset)
     return scaler.transform(dataset)
 
-def data_generation_mc():
-    all_parameters = parameters_mc()
+def data_generation_mc(samples, features):
+    all_parameters = parameters_mc(samples, features)
+    count = 0
     for n_samples, n_features, n_classes, p_informative in all_parameters : 
+        count += 1
         features, classes = make_classification(
             n_samples = n_samples, 
             n_features = n_features, 
@@ -33,10 +34,23 @@ def data_generation_mc():
             n_repeated = 0) 
         dataset = pd.DataFrame(scaler(features))
         dataset['class'] = classes
-        dataset.to_csv(f'./data/samples{n_samples}_features{n_features}_classes{n_classes}_informative{int(p_informative*100)}.csv')
-
+        dataset.to_csv(f'./data/mc_samples{n_samples}_features{n_features}_classes{n_classes}_informative{int(p_informative*100)}.csv')
+    print(f'{count} CSV files have been created with data following a normal distribution.')
+        
+def data_generation_uni(samples, features):
+    all_parameters = parameters_uni(samples, features)
+    count = 0
+    for n_samples, n_features in all_parameters : 
+        count += 1
+        dataset = pd.DataFrame(np.random.uniform(size=(n_samples, n_features)))
+        dataset.to_csv(f'./data/uni_samples{n_samples}_features{n_features}.csv')
+    print(f'{count} CSV files have been created with data following a uniform distribution.')
+        
 
 if not os.path.exists('data'):
     os.makedirs('data')
-data_generation_mc()
+samples = [100,200,300,500,1000,5000,10000]
+features = [10,20,30,40,50]
+data_generation_mc(samples, features)
+data_generation_uni(samples, features)
 
