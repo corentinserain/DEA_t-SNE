d6d68667ce65751b4eeaeef22832b79b1a7562f2
ecoutaud <131028820+ecoutaud@users.noreply.github.com>
2023-12-01 17:25:24 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 from generate_data import data_generation_mc, data_generation_uni
+from tsne import tsne_all
 
+#Generation des données
 samples = [100, 200, 400, 600, 800, 1000, 2000, 3000, 4000, 5000]
 features = [10, 20, 30, 40, 50, 80]
 classes = [1, 2, 3, 4, 5, 8, 10, 12, 15]
 informative = [0.5, 0.75, 1]
-folder = "../../../espaces/travail/Projet_DEA"
+folder = "./data"
 
 data_generation_mc(folder, 5, samples, features, classes, informative)
 data_generation_uni(folder, 5, samples, features)
+
+#t-sne
+perplexity = [10, 30, 50]
+n_iter = [300, 500, 1000]
+distr = ["uni", "cluster", "norm"]
+for i in distr :
+    input_folder = f"./data/{i}"
+    output_folder = f"./t_sne/{i}"
+    tsne_all(input_folder, output_folder, perplexity, n_iter)
+    print("T-sne was successfully applied to the data files with the distribution ", i)
---
+++ tsne.py
@@ -0,0 +1,28 @@
+from sklearn.manifold import TSNE
+import sklearn
+import pandas as pd
+import numpy as np
+from itertools import product
+
+import os
+
+
+
+def tsne(file, output_folder, perplexity, n_iter):
+    df = pd.read_csv(file, index_col=0)
+    all_parameters = list(product(perplexity,n_iter))
+    filename = os.path.basename(file)
+    filename, _ = os.path.splitext(filename)
+    for perplexity_val, n_iter_val in all_parameters:
+        tsne = sklearn.manifold.TSNE(perplexity = perplexity_val, n_iter = n_iter_val)
+        tsne.fit_transform(df)
+        if not os.path.exists(output_folder):
+            os.makedirs(output_folder)
+        df.to_csv(f'{output_folder}/{perplexity_val}_{n_iter_val}_{filename}.csv')
+
+def tsne_all(input_folder, output_folder, perplexity, n_iter):
+    for file in os.listdir(input_folder):
+        tsne(f'{input_folder}/{file}', output_folder, perplexity, n_iter)
+
+
+
