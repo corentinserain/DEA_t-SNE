80dc10ccc748ac51982ce83aae4a4f32575dbaa7
ecoutaud <131028820+ecoutaud@users.noreply.github.com>
2023-12-11 12:12:58 +0100

    Code propre

--- generate_data.py
+++ generate_data.py
@@ -2,7 +2,7 @@ from sklearn.datasets import make_classification
 from sklearn.preprocessing import MinMaxScaler
 from itertools import product
 import pandas as pd 
-import os
+import os 
 import numpy as np
 
 
@@ -18,30 +18,35 @@ def scaler(dataset):
     scaler = MinMaxScaler()
     scaler.fit(dataset)
     return scaler.transform(dataset)
-
+   
 
 def data_generation_mc(folder, rep, samples, features, classes, informative):
     """Generating of random data with the make_classification function with all given parameter combinations and creating folders in CSV format.
     When the number of classes is equal to 1, the data follows a normal distribution. 
     When the number of classes is higher than 1, the data are separated into clusters. 
     The name of the CSV folders created corresponds to the following form: 
-        distribution_samples_features_classes_(p_informative*100)_rep.csv
+        samples_features_classes_(p_informative*100)_rep.csv
 
     Args:
-        folder (str): The name of the folder where the CSV folders are generated.
-        rep (int):
+        folder (str): The name of the folder where the CSV files are generated.
+        rep (int): Number of repetitions of data generation.
         samples (list): Values for the number of samples to generate.
         features (list): Values for the number of features to generate.
         classes (list): Values defining the number of classes.
         informative (list): Proportion of features that are informative.
+
+    Returns:
+        list_norm (list): Names of CSV files created for normal distribution.
+        list_cluster (list): Names of CSV files created for cluster distribution.
     """
     all_parameters = list(product(samples, features, classes, informative))
-    count_norm = 0
-    count_cluster = 0
-    liste_norm = []
-    liste_cluster = []
-    if not os.path.exists(folder):
-        os.makedirs(folder)
+    list_norm = []
+    list_cluster = []
+    if not os.path.exists(f'{folder}/norm'):
+        os.makedirs(f'{folder}/norm')
+    if not os.path.exists(f'{folder}/cluster'):
+        os.makedirs(f'{folder}/cluster')
+    
     for i in range(rep) : 
         for n_samples, n_features, n_classes, p_informative in all_parameters : 
             name = f'{n_samples}_{n_features}_{n_classes}_{int(p_informative*100)}_{i}'
@@ -54,51 +59,49 @@ def data_generation_mc(folder, rep, samples, features, classes, informative):
                 n_repeated = 0)
             dataset = pd.DataFrame(scaler(features))
             dataset['class'] = classes
+
             if n_classes == 1 : 
-                if not os.path.exists(f'{folder}/norm'):
-                    os.makedirs(f'{folder}/norm')
-                count_norm += 1
-                dataset.to_csv(f'{folder}/norm/{name}.csv') 
-                liste_norm.append(name)
+                dataset.to_csv(f'{folder}/norm/{name}.csv')   
+                list_norm.append(name)
+
             else : 
-                if not os.path.exists(f'{folder}/cluster'):
-                    os.makedirs(f'{folder}/cluster')
-                count_cluster += 1
                 dataset.to_csv(f'{folder}/cluster/{name}.csv')
-                liste_cluster.append(name)
+                list_cluster.append(name)
  
-    print(f'{count_norm} CSV files have been created with data following a normal distribution.')
-    print(f'{count_cluster} CSV files have been created with data following a cluster distribution.')
-    return liste_norm, liste_cluster
+    print(f'{len(list_norm)} CSV files have been created with data following a normal distribution.')
+    print(f'{len(list_cluster)} CSV files have been created with data following a cluster distribution.')
+    return list_norm, list_cluster
         
 
 def data_generation_uni(folder, rep, samples, features):
-    """Generating of random data with the make_classification function with all given parameter combinations and creating folders in CSV format.
+    """Generating of random data with the random uniform function with all given parameter combinations and creating folders in CSV format.
     The data follows a uniform distribution.
     The name of the CSV folders created corresponds to the following form: 
-        distribution_samples_features_rep.csv
+        samples_features_rep.csv
 
     Args:
         folder (str): The name of the folder where the CSV folders are generated.
-        rep (int):
+        rep (int): Number of repetitions of data generation.
         samples (list): Values for the number of samples to generate.
         features (list): Values for the number of features to generate.
+
+    Returns:
+        list_uni (list): Names of CSV files created for uniform distribution.
     """
     all_parameters = list(product(samples, features))
-    count = 0
-    liste = []
+    list_uni = []
     if not os.path.exists(f'{folder}/uni'):
         os.makedirs(f'{folder}/uni')
+
     for i in range(rep) : 
         for n_samples, n_features in all_parameters : 
             name = f'{n_samples}_{n_features}_{i}'
-            count += 1
             dataset = pd.DataFrame(np.random.uniform(size=(n_samples, n_features)))
             dataset.to_csv(f'{folder}/uni/{name}.csv')
-            liste.append(name)
+            list_uni.append(name)
 
-    print(f'{count} CSV files have been created with data following a uniform distribution.')
-    return liste
+    print(f'{len(list_uni)} CSV files have been created with data following a uniform distribution.')
+    return list_uni
 
 
         
--- tsne.py
+++ tsne.py
@@ -1,50 +1,84 @@
-from sklearn.manifold import TSNE
+import os
 import sklearn
 import pandas as pd
 from itertools import product
+from sklearn.manifold import TSNE 
 from generate_data import scaler
 from metrics import average_common_neighbors, std_ratios
-import os
 
+def tsne(df, folder, perplexity, n_iter, classe):
+    """Apply the tsne algorithm on a single dataframe with all combinations of parameters (perplexity, number of iterations) 
+    and create a csv file with the tsne result for each combination. 
+    The name of the CSV folders created corresponds to the following form: 
+        perplexity_n_iter_samples_features_classes_(p_informative*100)_rep.csv
 
+    Args:
+        df (DataFrame): Data for which applied tsne.
+        folder (str): The name of the folder where the CSV files are generated.
+        perplexity (list): Values for the tsne parameter perplexity.
+        n_iter (list): Values for the tsne parameter n_iter.
+        classe (bool): When True, the dataframe contains a class column. 
 
-def tsne(df, output_folder, perplexity, n_iter, classe):
+    Returns:
+        tsne_results (list): All tsne results generated from the same data file.
+    """
     tsne_results = []
-    if classe :
-        y = df['class']
-        X = df.drop('class', axis=1) 
-    else : X = df
     all_parameters = list(product(perplexity,n_iter))
-    for perplexity_val, n_iter_val in all_parameters:
-        name = f'{perplexity_val}_{n_iter_val}_{df.name}'
-        tsne = sklearn.manifold.TSNE(perplexity = perplexity_val, n_iter = n_iter_val)
-        tsne_result = pd.DataFrame(scaler(tsne.fit_transform(X)))
-        if classe :
-            tsne_result['class'] = y
+    df_name = df.name
+    if not os.path.exists(folder):
+        os.makedirs(folder)
 
-        if not os.path.exists(output_folder):
-            os.makedirs(output_folder)
-        tsne_result.to_csv(f'{output_folder}/{name}.csv')
+    if classe :
+        classes = df['class']
+        df = df.drop('class', axis=1) 
+
+    for perplexity_val, n_iter_val in all_parameters:
+        name = f'{perplexity_val}_{n_iter_val}_{df_name}'
+        tsne = sklearn.manifold.TSNE(perplexity = perplexity_val, n_iter = n_iter_val)
+        tsne_result = pd.DataFrame(scaler(tsne.fit_transform(df)))
+        if classe :
+            tsne_result['class'] = classes
+
+        tsne_result.to_csv(f'{folder}/{name}.csv')
+
         tsne_result.name = name
         tsne_results.append(tsne_result)
+
     return tsne_results
 
-def tsne_metrics(data_liste, distr, perplexity, n_iter, classe):
+
+def tsne_metrics(data, distr, perplexity, n_iter, classe):
+    """Apply the tsne algorithm to all data files for a distribution, with all combinations of parameters (perplexity, number of iterations). 
+    Calculate two metrics(average common neighbors and ratios) based on the data file and its associated tsne result files. 
+    Create a csv file with a row for the metric results and parameters for each tsne result.
+
+    Args:
+        data (list): Data file names.
+        distr (str): Data distribution.
+        perplexity (list): Values for the tsne parameter perplexity.
+        n_iter (list): Values for the tsne parameter n_iter.
+        classe (bool): When True, the dataframe contains a class column. 
+
+    Returns:
+        df_metric (DataFrame): Metric results and parameters for each tsne result.
+    """
     if distr == "uni":
         columns = ["perplexity", "n_iter", "n_samples", "n_features", "avg_common_neighbors10", "avg_common_neighbors40", "std_ratios"]
     else : 
         columns = ["perplexity", "n_iter", "n_samples", "n_features", "n_classes", "p_informative", "avg_common_neighbors10", "avg_common_neighbors40", "std_ratios"]
+    
     df_metric = pd.DataFrame(columns=columns)
-    count = 1
-    for file_name in data_liste:
+    line = 1
+
+    for file_name in data:
         df_data = pd.read_csv(f'./data/{distr}/{file_name}.csv')
         df_data.name = file_name
         dfs_t_sne = tsne(df_data, f'./t_sne/{distr}', perplexity, n_iter, classe)
+
         for df_t_sne in dfs_t_sne : 
             name = df_t_sne.name
             param = name.split("_")
             del(param[-1])
-            
 
             if classe :
                 df_t_sne = df_t_sne.drop('class', axis=1) 
@@ -54,8 +88,8 @@ def tsne_metrics(data_liste, distr, perplexity, n_iter, classe):
 
             param.append(std_ratios(df_data, df_t_sne))
             
-            df_metric.loc[count]=param
-            count += 1
+            df_metric.loc[line]=param
+            line += 1
             
     df_metric.to_csv(f'metrics_results_{distr}.csv')
     return df_metric
